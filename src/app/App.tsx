652c32d78f68fa46ed2bbb7731be4eccad9e3dc1
NastyaZ23 <nastyh1233@gmail.com>
2023-03-09 17:18:21 +0300

    Feature sliced design

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { type ReturnComponentType } from 'shared/types/ReturnComponentType';
+import './index.css';
+
+const App = (): ReturnComponentType => {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/shared/types/ReturnComponentType.ts
@@ -0,0 +1,5 @@
+import { type ReactElement } from 'react';
+
+import { type Nullable } from './Nullable';
+
+export type ReturnComponentType = Nullable<ReactElement>;
